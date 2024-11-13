80de2a4178b6c005ef693ef25d90830ae0e1947e
Umberto Loria <umberto.loria@gmail.com>
2024-11-13 13:13:14 +0100

    Extracted similarity's main logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::main_factorization::main_factorization;
+use crate::suites::similarity::main_similarity;
 
 mod lyndon;
 mod jaccard;
@@ -7,56 +8,13 @@ mod debug;
 mod vector;
 mod fasta;
 mod main_factorization;
+mod suites;
 
 fn main() {
 
     // FACTORIZATIONS
     main_factorization();
 
-    /*
-    // JACCARD ON K-MERS
-    let mut bands = Vec::new();
-    bands.push("radiohead");
-    bands.push("slipknot");
-    bands.push("avengedsevenfold");
-    bands.push("systemofadown");
-    bands.push("threedaysgrace");
-    bands.push("breakingbenjamin");
-    bands.push("mrbungle");
-    bands.push("dreamtheater");
-    bands.push("porcupinetree");
-    bands.push("redhotchilipeppers");
-    bands.push("sum41");
-    bands.push("therecoveries");
-    let k = 1; // Considering single letters for now.
-
-    for i in 0..bands.len() {
-        for j in i..bands.len() {
-            let src1 = bands[i];
-            let src2 = bands[j];
-
-            let calculated_jaccard_similarity = jaccard_similarity_via_kmers(src1, src2, k);
-            println!("Similarity between \"{}\" and \"{}\" is: {}", src1, src2, calculated_jaccard_similarity);
-        }
-    }
-    */
-
-    /*
-    // JACCARD ON K-MERS OF FILES
-    let filepath1 = "in/1.fasta";
-    let contents1_buf = get_fasta_content(filepath1);
-    let contents1 = contents1_buf.as_str();
-
-    let filepath2 = "in/2.fasta";
-    let contents2_buf = get_fasta_content(filepath2);
-    let contents2 = contents2_buf.as_str();
-
-    let before = Instant::now();
-    for k in 1..141 {
-        let calculated_jaccard_similarity = jaccard_similarity_via_kmers(contents1, contents2, k);
-        println!("Similarity k={} is: {}", k, calculated_jaccard_similarity);
-    }
-    let after = Instant::now();
-    println!("Total time: {}", (after - before).as_secs_f32());
-    */
+    // SIMILARITY
+    // main_similarity();
 }
---
+++ src/suites/similarity.rs
@@ -0,0 +1,49 @@
+use crate::fasta::get_fasta_content;
+use crate::jaccard::jaccard_similarity_via_kmers;
+use std::time::Instant;
+
+pub fn main_similarity() {
+
+    // JACCARD ON K-MERS
+    let mut bands = Vec::new();
+    bands.push("radiohead");
+    bands.push("slipknot");
+    bands.push("avengedsevenfold");
+    bands.push("systemofadown");
+    bands.push("threedaysgrace");
+    bands.push("breakingbenjamin");
+    bands.push("mrbungle");
+    bands.push("dreamtheater");
+    bands.push("porcupinetree");
+    bands.push("redhotchilipeppers");
+    bands.push("sum41");
+    bands.push("therecoveries");
+    let k = 1; // Considering single letters for now.
+
+    for i in 0..bands.len() {
+        for j in i..bands.len() {
+            let src1 = bands[i];
+            let src2 = bands[j];
+
+            let calculated_jaccard_similarity = jaccard_similarity_via_kmers(src1, src2, k);
+            println!("Similarity between \"{}\" and \"{}\" is: {}", src1, src2, calculated_jaccard_similarity);
+        }
+    }
+
+    // JACCARD ON K-MERS OF FILES
+    let filepath1 = "in/1.fasta";
+    let contents1_buf = get_fasta_content(filepath1);
+    let contents1 = contents1_buf.as_str();
+
+    let filepath2 = "in/2.fasta";
+    let contents2_buf = get_fasta_content(filepath2);
+    let contents2 = contents2_buf.as_str();
+
+    let before = Instant::now();
+    for k in 1..141 {
+        let calculated_jaccard_similarity = jaccard_similarity_via_kmers(contents1, contents2, k);
+        println!("Similarity k={} is: {}", k, calculated_jaccard_similarity);
+    }
+    let after = Instant::now();
+    println!("Total time: {}", (after - before).as_secs_f32());
+}
