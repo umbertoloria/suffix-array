34421196e5a61a618696bc3a0860409732cba5c7
Umberto Loria <umberto.loria@gmail.com>
2024-12-18 13:44:13 +0100

    Suffix Array: Ranking List ok all Local Suffixes ok

--- src/suites/suffix_array.rs
+++ src/suites/suffix_array.rs
@@ -7,54 +7,47 @@ pub fn main_suffix_array() {
     println!("ICFL: {:?}", factors);
 
     let mut local_suffixes = Vec::new();
-    for factor in &factors {
-        for i in 0..factor.len() {
-            let local_suffix = factor[i..factor.len()].to_string();
+    let mut map: HashMap<String, Vec<usize>> = HashMap::new();
+
+    // FACTOR OFFSETS
+    let mut factor_offsets = Vec::new();
+    let mut offset = 0;
+    for i in 0..factors.len() {
+        factor_offsets.push(offset);
+        offset += factors[i].len();
+    }
+
+    // Closure to address one factor's Local Suffixes
+    let mut compute_factor_local_suffixes = |i_factor: usize| {
+        let factor = &factors[i_factor];
+        // println!("managing {}", factor);
+        for i in (0..factor.len()).rev() {
+            let local_suffix = &factor[i..factor.len()];
+            // println!(" -> {} {} -> {}", i, factor.len(), local_suffix);
             if !local_suffixes.contains(&local_suffix) {
                 local_suffixes.push(local_suffix);
-                // println!("{:?}", local_suffix);
             }
+            if !map.contains_key(local_suffix) {
+                map.insert(local_suffix.to_string(), Vec::new());
+            }
+            let index_in_whole_string = factor_offsets[i_factor] + i;
+            map.get_mut(local_suffix)
+                .unwrap()
+                .splice(0..0, [index_in_whole_string]);
         }
+    };
+
+    // Computing all factors
+    for i in (0..factors.len() - 1).rev() {
+        compute_factor_local_suffixes(i);
     }
+    compute_factor_local_suffixes(factors.len() - 1);
+
     local_suffixes.sort();
     println!("LOC SUFFXS: {:?}", local_suffixes);
 
-    let mut map: HashMap<String, Vec<usize>> = HashMap::new();
-    for local_suffix in &local_suffixes {
-        println!("{}", local_suffix);
-
-        // Factors: from second last to first
-        for i in 1..factors.len() {
-            let factor = &factors[factors.len() - 1 - i];
-            if factor.len() >= local_suffix.len() {
-                let left = factor.len() - local_suffix.len();
-                let factor_suffix = &factor[left..factor.len()];
-                if factor_suffix.eq(local_suffix) {
-                    if !map.contains_key(local_suffix) {
-                        map.insert(local_suffix.to_string(), vec![]);
-                    }
-                    map.get_mut(local_suffix).unwrap().push(75);
-                    // println!(" ---> {factor_suffix} !!!");
-                }
-            }
-        }
-
-        // Factors: last
-        let factor = &factors[factors.len() - 1];
-        if factor.len() >= local_suffix.len() {
-            let left = factor.len() - local_suffix.len();
-            let factor_suffix = &factor[left..factor.len()];
-            if factor_suffix.eq(local_suffix) {
-                if !map.contains_key(local_suffix) {
-                    map.insert(local_suffix.to_string(), vec![]);
-                }
-                map.get_mut(local_suffix).unwrap().push(75);
-                // println!(" ---> {factor_suffix} !!!");
-            }
-        }
-    }
-    println!("MAP:\n");
-    for local_suffix in &local_suffixes {
+    println!("MAP:");
+    for local_suffix in local_suffixes {
         let item = map.get(local_suffix).unwrap();
         println!("{} -> {:?}", local_suffix, item);
     }
