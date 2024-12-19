6cebaf18516c40499ebf18ca7be1143af4cf4604
Umberto Loria <umberto.loria@gmail.com>
2024-12-19 17:21:31 +0100

    Ranking List improved

--- src/suites/suffix_array.rs
+++ src/suites/suffix_array.rs
@@ -4,12 +4,8 @@ use std::collections::HashMap;
 pub fn main_suffix_array() {
     let src = "aaabcaabcadcaabca";
     let factors = icfl(src);
-    println!("ICFL: {:?}", factors);
 
-    let mut local_suffixes = Vec::new();
-    let mut map: HashMap<String, Vec<usize>> = HashMap::new();
-
-    // FACTOR OFFSETS
+    // Factor offsets
     let mut factor_offsets = Vec::new();
     let mut offset = 0;
     for i in 0..factors.len() {
@@ -17,21 +13,27 @@ pub fn main_suffix_array() {
         offset += factors[i].len();
     }
 
+    let mut ls_list = Vec::new();
+    let mut ls_rankings_map: HashMap<String, Vec<usize>> = HashMap::new();
     // Closure to address one factor's Local Suffixes
     let mut compute_factor_local_suffixes = |i_factor: usize| {
         let factor = &factors[i_factor];
-        // println!("managing {}", factor);
         for i in (0..factor.len()).rev() {
-            let local_suffix = &factor[i..factor.len()];
-            // println!(" -> {} {} -> {}", i, factor.len(), local_suffix);
-            if !local_suffixes.contains(&local_suffix) {
-                local_suffixes.push(local_suffix);
+            // Local suffix "s"
+            let s = &factor[i..factor.len()];
+
+            // Add "s" to Local Suffixes list
+            if !ls_list.contains(&s) {
+                ls_list.push(s);
             }
-            if !map.contains_key(local_suffix) {
-                map.insert(local_suffix.to_string(), Vec::new());
-            }
+
+            // Add [s]-ranking
             let index_in_whole_string = factor_offsets[i_factor] + i;
-            map.get_mut(local_suffix)
+            if !ls_rankings_map.contains_key(s) {
+                ls_rankings_map.insert(s.into(), Vec::new());
+            }
+            ls_rankings_map
+                .get_mut(s)
                 .unwrap()
                 .splice(0..0, [index_in_whole_string]);
         }
@@ -43,12 +45,19 @@ pub fn main_suffix_array() {
     }
     compute_factor_local_suffixes(factors.len() - 1);
 
-    local_suffixes.sort();
-    println!("LOC SUFFXS: {:?}", local_suffixes);
+    // Finalizing "ls_list" and "ls_rankings"
+    ls_list.sort();
+    let mut ls_rankings_list = Vec::new();
+    for s_index in 0..ls_list.len() {
+        let s = ls_list[s_index];
+        let s_ranking = ls_rankings_map.get(s).unwrap();
+        ls_rankings_list.push(s_ranking);
+    }
 
-    println!("MAP:");
-    for local_suffix in local_suffixes {
-        let item = map.get(local_suffix).unwrap();
-        println!("{} -> {:?}", local_suffix, item);
+    // Output
+    for s_index in 0..ls_list.len() {
+        let s = ls_list[s_index];
+        let s_ranking = ls_rankings_map.get(s).unwrap();
+        println!("{s} -> {s_ranking:?}");
     }
 }
