e9d0b32f8a1db27dba821570d0d27940d1b16d59
Umberto Loria <umberto.loria@gmail.com>
2025-01-22 17:07:47 +0100

    Innovative Suffix Array calculation with Chronometer

--- src/suites/suffix_array.rs
+++ src/suites/suffix_array.rs
@@ -11,64 +11,17 @@ use std::time::{Duration, Instant};
 pub fn main_suffix_array() {
     let src = get_fasta_content("generated/001.fasta".into());
     let src_str = src.as_str();
-    let src_length = src.len();
     // println!("STRING={}", src_str);
 
-    // Compute ICFL
-    let factors = icfl(src_str);
-
-    let chunk_size = 3;
-    println!("chunk_size={}", chunk_size);
-    // TODO: Simplify algorithms by having string length as last item of these Factor Index vectors
-    let icfl_indexes = get_indexes_from_factors(&factors);
-    println!("ICFL_INDEXES={:?}", icfl_indexes);
-    println!("ICFL FACTORS: {:?}", factors);
-
-    let custom_indexes = get_custom_factors(&icfl_indexes, chunk_size, src_length);
-    let custom_factors = get_custom_factor_strings_from_custom_indexes(src_str, &custom_indexes);
-    println!("CSTM_INDEXES={:?}", custom_indexes);
-    println!("CSTM FACTORS: {:?}", custom_factors);
-
-    // let max_size = get_max_size(&icfl_indexes, src_length).expect("max_size is not valid");
-    // println!("MAX_SIZE={:?}", max_size);
-    // println!("CSTM_MAX_SIZE={:?}", custom_max_size);
-
-    // TODO: Optimize both functions and rename them (and variables)
-    // Factor List: [Source Char Index] => True if it's part of the last Custom Factor of an
-    //                                     ICFL Factor, so it's a Local Suffix
-    let is_custom_vec = get_is_custom_vec(&icfl_indexes, src_length, chunk_size);
-    println!("is_custom_vec={:?}", is_custom_vec);
-
-    // Factor List: [Source Char Index] => ICFL Factor Index of that
-    let factor_list = get_factor_list(&icfl_indexes, src_length);
-    println!("factor_list={:?}", factor_list);
-
-    // Prefix Trie Structure create
-    let mut root = create_prefix_trie(&src, src_length, &custom_indexes, &is_custom_vec);
-
-    // Ordering rankings.
-    println!("Before merge");
-    root.print(0, "".into());
-
-    // Merge Rankings (Canonical and Custom)
-    let mut wbsa = (0..src_length).collect::<Vec<_>>();
-    root.merge_rankings_and_sort_recursive(src_str, &mut wbsa, 0);
-    println!("WANNA BE SUFFIX ARRAY: {:?}", &wbsa);
-
-    println!("Before SHRINK");
-    root.print_with_wbsa(0, "".into(), &wbsa);
-
-    root.shrink_bottom_up(
-        &mut wbsa,
-        src_str,
-        &icfl_indexes,
-        &is_custom_vec,
-        &factor_list,
+    // INNOVATIVE SUFFIX ARRAY
+    let innovative_suffix_array_computation = compute_innovative_suffix_array(src_str, false);
+    let wbsa = innovative_suffix_array_computation.suffix_array;
+    println!("INNOVATIVE SUFFIX ARRAY CALCULATION");
+    println!(
+        " > Duration: {} seconds",
+        innovative_suffix_array_computation.duration.as_secs_f64()
     );
-
-    println!("After SHRINK");
-    root.print_with_wbsa(0, "".into(), &wbsa);
-    println!("{:?}", wbsa);
+    // println!(" > Suffix Array: {:?}", wbsa);
 
     // CLASSIC SUFFIX ARRAY
     println!();
@@ -82,6 +35,7 @@ pub fn main_suffix_array() {
     // println!(" > Suffix Array: {:?}", classic_suffix_array);
 
     // VERIFICATION
+    println!();
     if wbsa.len() != classic_suffix_array.len() {
         println!("Wanna Be Suffix Array is insufficient in size");
     } else {
@@ -138,6 +92,81 @@ fn compute_classic_suffix_array(
     ClassicSuffixArrayComputationResults {
         suffix_array: suffix_array_indexes,
         suffix_array_pairs,
+        duration: after - before,
+    }
+}
+
+// INNOVATIVE SUFFIX ARRAY
+struct InnovativeSuffixArrayComputationResults {
+    suffix_array: Vec<usize>,
+    duration: Duration,
+}
+fn compute_innovative_suffix_array(
+    str: &str,
+    debug_verbose: bool,
+) -> InnovativeSuffixArrayComputationResults {
+    let before = Instant::now();
+
+    let src_length = str.len();
+
+    // Compute ICFL
+    let factors = icfl(str);
+
+    let chunk_size = 3;
+    // TODO: Simplify algorithms by having string length as last item of these Factor Index vectors
+    let icfl_indexes = get_indexes_from_factors(&factors);
+    let custom_indexes = get_custom_factors(&icfl_indexes, chunk_size, src_length);
+    let custom_factors = get_custom_factor_strings_from_custom_indexes(str, &custom_indexes);
+
+    // TODO: Optimize both functions and rename them (and variables)
+    // Factor List: [Source Char Index] => True if it's part of the last Custom Factor of an
+    //                                     ICFL Factor, so it's a Local Suffix
+    let is_custom_vec = get_is_custom_vec(&icfl_indexes, src_length, chunk_size);
+
+    // Factor List: [Source Char Index] => ICFL Factor Index of that
+    let factor_list = get_factor_list(&icfl_indexes, src_length);
+
+    if debug_verbose {
+        println!("chunk_size={}", chunk_size);
+        println!("ICFL_INDEXES={:?}", icfl_indexes);
+        println!("ICFL FACTORS: {:?}", factors);
+        println!("CSTM_INDEXES={:?}", custom_indexes);
+        println!("CSTM FACTORS: {:?}", custom_factors);
+        println!("is_custom_vec={:?}", is_custom_vec);
+        println!("factor_list={:?}", factor_list);
+    }
+
+    // Prefix Trie Structure create
+    let mut root = create_prefix_trie(str, src_length, &custom_indexes, &is_custom_vec);
+
+    if debug_verbose {
+        println!("Before merge");
+        root.print(0, "".into());
+    }
+
+    // Merge Rankings (Canonical and Custom)
+    let mut wbsa = (0..src_length).collect::<Vec<_>>();
+    root.merge_rankings_and_sort_recursive(str, &mut wbsa, 0);
+
+    if debug_verbose {
+        println!("Before SHRINK");
+        root.print_with_wbsa(0, "".into(), &wbsa);
+    }
+
+    root.shrink_bottom_up(&mut wbsa, str, &icfl_indexes, &is_custom_vec, &factor_list);
+
+    if debug_verbose {
+        println!("After SHRINK");
+        root.print_with_wbsa(0, "".into(), &wbsa);
+        println!("{:?}", wbsa);
+    }
+
+    let after = Instant::now();
+
+    // println!("Total time: {}", duration.as_secs_f32());
+
+    InnovativeSuffixArrayComputationResults {
+        suffix_array: wbsa,
         duration: after - before,
     }
 }
