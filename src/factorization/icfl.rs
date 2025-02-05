0eeffeaf951cf9471bd099dddf286ab84241a2f0
Umberto Loria <umberto.loria@gmail.com>
2025-02-05 18:37:23 +0100

    ICFL improved

--- src/factorization/icfl.rs
+++ src/factorization/icfl.rs
@@ -2,12 +2,11 @@ use std::i32;
 
 pub fn icfl(s: &str) -> Vec<String> {
     // NOTE: Works using chars as bytes.
-    let factors = icfl_bytes(s.as_bytes());
-
     let mut result = Vec::new();
-    for i in 0..factors.len() {
-        let factor_bytes = &factors[i];
-        let factor = String::from_utf8(factor_bytes.to_vec()).unwrap();
+
+    let factors_in_bytes = icfl_bytes(s.as_bytes());
+    for factor_bytes in factors_in_bytes {
+        let factor = String::from_utf8(factor_bytes).unwrap();
         result.push(factor);
     }
 
@@ -20,7 +19,9 @@ pub fn icfl_bytes(w: &[u8]) -> Vec<Vec<u8>> {
     // if x == w + '0' // Should be.
     if x.len() == w.len() + 1 && x[w.len()] == '0'.try_into().unwrap() {
         let mut i = 0;
-        while i < w.len() && x[i] == w[i] { i += 1; }
+        while i < w.len() && x[i] == w[i] {
+            i += 1;
+        }
         if i == w.len() {
             return [w.to_vec()].to_vec();
         }
@@ -31,7 +32,8 @@ pub fn icfl_bytes(w: &[u8]) -> Vec<Vec<u8>> {
     let mut bre_plus_y = bre.clone();
     bre_plus_y.extend(y);
     let mut l = icfl_bytes(bre_plus_y.as_slice());
-    if l[0].len() > last.try_into().unwrap() { // |m1'| > |r|
+    if l[0].len() > last.try_into().unwrap() {
+        // |m1'| > |r|
         l.insert(0, p);
     } else {
         // l[0] = p + l[0]; // Should be.
@@ -72,9 +74,13 @@ pub fn icfl_find_prefix(w: &[u8]) -> (Vec<u8>, Vec<u8>) {
     }
 
     let mut res1 = Vec::new();
-    for i in 0..j + 1 { res1.push(w[i]); }
+    for i in 0..j + 1 {
+        res1.push(w[i]);
+    }
     let mut res2 = Vec::new();
-    for i in j + 1..w.len() { res2.push(w[i]); }
+    for i in j + 1..w.len() {
+        res2.push(w[i]);
+    }
     // return (w[:j+1], w[j+1:])
     (res1, res2)
 }
@@ -82,8 +88,12 @@ pub fn icfl_find_prefix(w: &[u8]) -> (Vec<u8>, Vec<u8>) {
 pub fn icfl_find_bre(x: &[u8], y: &[u8]) -> (Vec<u8>, Vec<u8>, i32) {
     // TODO: Improve conversion logics
     let mut w = Vec::with_capacity(x.len() + y.len());
-    for i in 0..x.len() { w.push(x[i]); }
-    for i in 0..y.len() { w.push(y[i]); }
+    for i in 0..x.len() {
+        w.push(x[i]);
+    }
+    for i in 0..y.len() {
+        w.push(y[i]);
+    }
     let w = w.as_slice();
 
     let n = i32::try_from(x.len() - 1).unwrap();
@@ -112,16 +122,18 @@ pub fn icfl_find_bre(x: &[u8], y: &[u8]) -> (Vec<u8>, Vec<u8>, i32) {
 
     let mut res1 = Vec::with_capacity(sep1_usize);
     let mut res2 = Vec::with_capacity(
-        usize::try_from(
-            i32::max(
-                i32::try_from(sep2_usize).unwrap()
-                    - i32::try_from(sep1_usize).unwrap(),
-                0,
-            ),
-        ).unwrap(),
+        usize::try_from(i32::max(
+            i32::try_from(sep2_usize).unwrap() - i32::try_from(sep1_usize).unwrap(),
+            0,
+        ))
+        .unwrap(),
     );
-    for i in 0..sep1_usize { res1.push(w[i]); }
-    for i in sep1_usize..sep2_usize { res2.push(w[i]); }
+    for i in 0..sep1_usize {
+        res1.push(w[i]);
+    }
+    for i in sep1_usize..sep2_usize {
+        res2.push(w[i]);
+    }
 
     (res1, res2, last + 1)
 }
@@ -133,7 +145,9 @@ pub fn icfl_get_failure_function(s: &[u8], s_inner_size: usize) -> Vec<usize> {
     let m = s_inner_size;
 
     let mut f = Vec::with_capacity(m);
-    for _ in 0..m { f.push(0); }
+    for _ in 0..m {
+        f.push(0);
+    }
 
     let mut i = 1;
     let mut j = 0;
