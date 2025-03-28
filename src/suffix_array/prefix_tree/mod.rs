dd7779b8ba3d791ddbf43bc35861498cf34227de
Umberto Loria <umberto.loria@gmail.com>
2025-03-28 16:42:01 +0100

    Prefix Tree with Logger logics extracted

--- src/suffix_array/prefix_tree/mod.rs
+++ src/suffix_array/prefix_tree/mod.rs
@@ -1,2 +1,3 @@
 pub mod prefix_tree;
 pub mod prefix_tree_create;
+pub mod prefix_tree_logger;
--- src/suffix_array/prefix_tree/prefix_tree.rs
+++ src/suffix_array/prefix_tree/prefix_tree.rs
@@ -62,7 +62,7 @@ pub struct PrefixTreeNode {
     pub max_father: Option<usize>,
 }
 impl PrefixTreeNode {
-    fn get_label_from_first_ranking<'a>(&self, str: &'a str, rankings: &[usize]) -> &'a str {
+    pub fn get_label_from_first_ranking<'a>(&self, str: &'a str, rankings: &[usize]) -> &'a str {
         // Unfortunately (maybe), each caller has its own reason to already have "rankings"...
         let first_ranking = rankings[0];
         &str[first_ranking..first_ranking + self.suffix_len]
@@ -371,46 +371,6 @@ impl PrefixTreeNode {
         }
 
         result
-    }
-}
-
-// PREFIX TREE LOGGER
-pub fn log_prefix_tree(
-    prefix_tree: &PrefixTree,
-    str: &str,
-    prog_sa: &ProgSuffixArray,
-    filepath: String,
-) {
-    let mut file = File::create(filepath).expect("Unable to create file");
-    for child in &prefix_tree.children {
-        log_prefix_tree_recursive(child, str, prog_sa, &mut file, 0);
-    }
-    file.flush().expect("Unable to flush file");
-}
-fn log_prefix_tree_recursive(
-    node: &PrefixTreeNode,
-    str: &str,
-    prog_sa: &ProgSuffixArray,
-    file: &mut File,
-    level: usize,
-) {
-    let rankings = prog_sa.get_rankings(node.index);
-    let mut line = format!(
-        "{}{}",
-        " ".repeat(level),
-        node.get_label_from_first_ranking(str, rankings)
-    );
-    line.push_str(" [");
-    let last_ranking = rankings[rankings.len() - 1];
-    for i in 0..rankings.len() - 1 {
-        let ranking = rankings[i];
-        line.push_str(&format!("{}, ", ranking));
-    }
-    line.push_str(&format!("{}]", last_ranking));
-    line.push_str("\n");
-    file.write(line.as_bytes()).expect("Unable to write line");
-    for child in &node.children {
-        log_prefix_tree_recursive(child, str, prog_sa, file, level + 1);
     }
 }
 
