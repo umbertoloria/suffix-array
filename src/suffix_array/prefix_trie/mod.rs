10554afca196c0965fd2dffc655609c723bb9f5e
Umberto Loria <umberto.loria@gmail.com>
2025-03-28 16:35:16 +0100

    Prefix Trie with Print logics extracted

--- src/suffix_array/prefix_trie/mod.rs
+++ src/suffix_array/prefix_trie/mod.rs
@@ -1,3 +1,4 @@
 pub mod prefix_trie;
 pub mod prefix_trie_create;
 pub mod prefix_trie_logger;
+pub mod prefix_trie_print;
--- src/suffix_array/prefix_trie/prefix_trie.rs
+++ src/suffix_array/prefix_trie/prefix_trie.rs
@@ -38,64 +38,11 @@ impl<'a> PrefixTrie<'a> {
     }
 
     // Rankings
-    fn get_rankings_canonical(&self) -> &Vec<usize> {
+    pub fn get_rankings_canonical(&self) -> &Vec<usize> {
         &self.rankings_canonical
     }
-    fn get_rankings_custom(&self) -> &Vec<usize> {
+    pub fn get_rankings_custom(&self) -> &Vec<usize> {
         &self.rankings_custom
-    }
-
-    // Prints
-    pub fn print(&self, tabs_offset: usize, prefix_rec: String) {
-        println!(
-            "{}|{:2}: \"{}\" {}",
-            "\t".repeat(tabs_offset),
-            tabs_offset,
-            prefix_rec,
-            format!(
-                "{:?} {:?}",
-                self.get_rankings_canonical(),
-                self.get_rankings_custom()
-            ),
-        );
-        match &self.data {
-            PrefixTrieData::Children(children) => {
-                for (char_key, child_node) in children {
-                    let prefix_str = get_string_char_clone(*char_key);
-                    child_node.print(tabs_offset + 1, format!("{}{}", prefix_rec, prefix_str));
-                }
-            }
-            PrefixTrieData::DirectChild((prefix, child_node)) => {
-                let prefix_str = get_string_clone(prefix);
-                child_node.print(tabs_offset + 1, format!("{}{}", prefix_rec, prefix_str));
-            }
-            PrefixTrieData::Leaf => {}
-            PrefixTrieData::InitRoot => {}
-        }
-    }
-    pub fn print_merged(&self, tabs_offset: usize, prefix_rec: String) {
-        println!(
-            "{}\"{}\" {:?}",
-            "\t".repeat(tabs_offset),
-            prefix_rec,
-            self.rankings,
-        );
-        match &self.data {
-            PrefixTrieData::Children(children) => {
-                for (char_key, child_node) in children {
-                    let prefix_str = get_string_char_clone(*char_key);
-                    let prefix_rec = format!("{}{}", prefix_rec, prefix_str);
-                    child_node.print_merged(tabs_offset + 1, prefix_rec);
-                }
-            }
-            PrefixTrieData::DirectChild((prefix, child_node)) => {
-                let prefix_str = get_string_clone(prefix);
-                let prefix_rec = format!("{}{}", prefix_rec, prefix_str);
-                child_node.print_merged(tabs_offset + 1, prefix_rec);
-            }
-            PrefixTrieData::Leaf => {}
-            PrefixTrieData::InitRoot => {}
-        }
     }
 
     // Tree transformation
