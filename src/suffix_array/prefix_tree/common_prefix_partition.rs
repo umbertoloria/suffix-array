ca04595fa94aa6a958d026d431544a92b8958655
Umberto Loria <umberto.loria@gmail.com>
2025-05-06 18:30:06 +0200

    Common Prefix Partition improved

--- src/suffix_array/prefix_tree/common_prefix_partition.rs
+++ src/suffix_array/prefix_tree/common_prefix_partition.rs
@@ -22,31 +22,47 @@ impl<'a> Tree<'a> {
         for &(_, child_node_id) in &self_node.children {
             let child_node = self.get_node(child_node_id).borrow_mut();
             let child_cpp = self.get_common_prefix_partition(&child_node, verbose);
+
+            // Add all Self Node's Rankings.
             if let Some(min_father) = child_node.min {
                 if verbose {
-                    println!("Here self=?? and child=??");
+                    // Unfortunately, we don't have "self_node_id" :(
+                    println!("Here self=?? and child={child_node_id}");
                 }
-                if min_father >= position {
+
+                // There is a Min Father, so use all Self Node's Rankings remained (from "position")
+                // until "min_father" position (if there are some).
+                if position < min_father {
+                    // There are some Self Node's Rankings from "position" to "min_father".
                     result.extend(&this_rankings[position..min_father]);
-                }
-                result.extend(child_cpp);
-                if let Some(max_father) = child_node.max {
-                    position = max_father;
-                } else {
                     position = min_father;
                 }
+                if let Some(max_father) = child_node.max {
+                    // Here, there is both a Min Father and a Max Father, this means that there
+                    // exist a Window for Comparing Rankings using "RULES". This means that all
+                    // Self Node's Rankings from Min Father to Max Father should be ignored since
+                    // they were duplicated (during In-prefix Merge Phase) somewhere inside its
+                    // Children's Rankings, so they will be dealt with later (when these Children
+                    // Nodes become Self Nodes for this procedure).
+                    position = max_father;
+                }
             } else {
-                // Min Father is None.
+                // Min Father is None. There's no Window for Comparing Rankings using "RULES". This
+                // means that:
+                // all Self Node's Rankings have LSs that are < than all Child Node's Rankings.
+                // So we take firstly Self Node's Rankings, and then all the Child Node's Rankings.
                 result.extend(&this_rankings[position..]);
-                result.extend(child_cpp);
                 position = this_rankings.len();
             }
+
+            // Add all Child Node's Rankings.
+            result.extend(child_cpp);
         }
 
         result.extend(&this_rankings[position..]);
 
         if verbose {
-            // Here we don't have "self_node_id" :(
+            // Unfortunately, we don't have "self_node_id" :(
             println!(
                 "Node ID=? (m={:?}, M={:?}) {:?} => {:?}",
                 self_node.min, self_node.max, this_rankings, result
