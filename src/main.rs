47d7cbf722a8ae35be4385f0660faef086e78b3e
Umberto Loria <umberto.loria@gmail.com>
2025-06-13 11:42:04 +0200

    000 FASTA Project updated

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn main() {
     // main_factorization();
 
     // Chunk Size Interval
+    let chunk_size_vec_000 = create_chunk_size_interval_and_none(2, 7);
     let chunk_size_vec = create_chunk_size_interval(1, 50);
     // let chunk_size_vec = create_chunk_size_interval(5, 200);
     // let chunk_size_vec = create_chunk_size_interval(5, 30);
@@ -29,10 +30,10 @@ fn main() {
     let lftsa = false;
     // let lftsa = true;
 
-    // suite_complete_on_fasta_file("000", &chunk_size_vec, 25, le, lftsa);
+    suite_complete_on_fasta_file("000", &chunk_size_vec_000, 25, le, lftsa);
     // suite_complete_on_fasta_file("001", &chunk_size_vec, 25, le, lftsa);
     // suite_complete_on_fasta_file("002_mini", &chunk_size_vec, 30, le, lftsa);
-    suite_complete_on_fasta_file("002_70", &chunk_size_vec, 70_000, le, lftsa);
+    // suite_complete_on_fasta_file("002_70", &chunk_size_vec, 70_000, le, lftsa);
     // suite_complete_on_fasta_file("002_70", &chunk_size_vec, 200_000, le, lftsa);
     // suite_complete_on_fasta_file("002_70", &chunk_size_none_list, 200_000, le, lftsa);
     // suite_complete_on_fasta_file("002_700", &chunk_size_vec, 2_100_000, le, lftsa);
@@ -42,6 +43,12 @@ fn main() {
 
 fn create_chunk_size_interval(min: usize, max: usize) -> Vec<Option<usize>> {
     (min..=max).map(|x| Some(x)).collect()
+}
+
+fn create_chunk_size_interval_and_none(min: usize, max: usize) -> Vec<Option<usize>> {
+    let mut result = create_chunk_size_interval(min, max);
+    result.push(None);
+    result
 }
 
 fn create_chunk_size_of_thousands_with_steps(min: usize, max: usize) -> Vec<Option<usize>> {
